141ec2c85009eaa3e8a60d55967de328d3439b9d
afsana-haque <afsana.haque3301@gmail.com>
2023-05-20 04:18:35 +0600

    added error page

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -4,11 +4,13 @@ import Home from "../pages/Home/Home/Home";
 import Login from "../pages/Login/Login";
 import SignUp from "../pages/SignUp/SignUp";
 import Blog from "../pages/Blog/Blog";
+import ErrorPage from "../pages/ErrorPage/ErrorPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <MainLayout></MainLayout>,
+    errorElement: <ErrorPage></ErrorPage>,
     children: [
       {
         path: '/',
