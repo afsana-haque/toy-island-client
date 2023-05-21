9bc36d23c7d2b13c3d6b0b8aa1a24e6a05b8f936
afsana-haque <afsana.haque3301@gmail.com>
2023-05-21 20:48:38 +0600

    added single card

--- src/pages/Home/SingleToy/SingleToy.jsx
+++ src/pages/Home/SingleToy/SingleToy.jsx
@@ -4,28 +4,35 @@ import { useParams } from 'react-router-dom';
 import SingleToyCard from './SingleToyCard';
 
 const SingleToy = () => {
-    const {id} = useParams();
+    const { id } = useParams();
     console.log(id)
     const [singleToys, setSingleToys] = useState([]);
+    const { picture, name, seller, email, price, rating, quantity, description } = singleToys;
 
     useEffect(() => {
         fetch(`https://toy-island-server.vercel.app/allToy/${id}`)
-        .then(res => res.json())
-        .then(data => {
-            setSingleToys(data);
-            console.log(setSingleToys)
-        })
-    },[])
+            .then(res => res.json())
+            .then(data => {
+                setSingleToys(data);
+                console.log(data)
+            })
+    }, [])
     return (
         <div>
             <h2 className='text-3xl text-center'>Toys Details</h2>
-            <div>
-                {
-                    singleToys.map(singleToy => <SingleToyCard
-                    key={singleToy._id}
-                    singleToy={singleToy}
-                    ></SingleToyCard>)
-                }
+            <div className='m-12'>
+                <div className="card lg:card-side bg-base-100 shadow-xl">
+                    <figure><img src={picture} alt="Album" /></figure>
+                    <div className="card-body">
+                        <h2 className="card-title">{name}</h2>
+                        <p> Seller Name: {seller}</p>
+                        <p> Seller email: {email}</p>
+                        <p> Price: ${price}</p>
+                        <p> Rating: {rating}</p>
+                        <p> Quantity: {quantity}</p>
+                        <p> Description: {description}</p>
+                    </div>
+                </div>
             </div>
         </div>
     );
